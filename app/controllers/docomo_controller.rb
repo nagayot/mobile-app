afa351e94bc3d3e5c431af443860b5b7ac8e5c0c
nagayot <nagayot0220@gmail.com>
2020-06-14 19:09:38 +0900

    詳細ページ用にコントローラーの設定

--- app/controllers/docomo_controller.rb
+++ app/controllers/docomo_controller.rb
@@ -1,4 +1,28 @@
 class DocomoController < ApplicationController
   def index
   end
+
+  def Xperia10II
+
+  end
+
+  def GalaxyA41
+
+  end
+
+  def LGstyle3
+
+  end
+
+  def AQUOSsense3
+
+  end
+
+  def GalaxyA20
+
+  end
+
+  def arrowsBe4
+    
+  end
 end
